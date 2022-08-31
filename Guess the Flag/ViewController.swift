a6c07ea201858eba9aae6c3cb82b16d83f6b92df
Ali Demirtaş <alidemirtas.7@icloud.com>
2022-08-31 17:21:14 +0300

    done

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+        askQuestion()
+       
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        
     }
-
-
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+      
+        title = "\(countries[correctAnswer].uppercased())     Score = \(score)"
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            navigationItem.title = "\(countries[correctAnswer].uppercased())     Score = \(score)"
+        }else{
+            title = "Wrong"
+            score -= 1
+            navigationItem.title = "\(countries[correctAnswer].uppercased())     Score = \(score)"
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: { action in
+            self.askQuestion()
+        }))
+        present(ac,animated: true)
+    }
+    
 }
 
